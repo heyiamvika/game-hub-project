1d4bbf7376a6dfdc10b40feca2d33e836b10e31d
Ada <ada@Adas-MacBook-Pro.local>
2023-11-19 13:33:14 +0200

    Initial data fetching from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+interface Game {
+  id: string;
+  name: string;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [games, setGames] = useState<Game[]>([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.rawg.io/api/games?key=${import.meta.env.VITE_RAWG_API_KEY}`
+      )
+      .then((res) => {
+        setGames(res.data.results);
+      })
+      .catch((err) => {
+        console.error("Failed to fetch games: " + err.message);
+      });
+  }, []);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {games.map((game) => (
+          <p key={game.id}>{game.name}</p>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
