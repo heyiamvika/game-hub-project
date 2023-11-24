484c1aeceef5b6ac8038e1bdfc64baaf38c319ba
Ada <ada@Adas-MacBook-Pro.local>
2023-11-24 21:01:17 +0200

    Added skeleton to GameCard

--- src/components/games/GameCard.tsx
+++ src/components/games/GameCard.tsx
@@ -8,8 +8,10 @@ import {
   Icon,
   HStack,
   VStack,
+  Skeleton,
 } from "@chakra-ui/react";
 import {} from "@chakra-ui/icons";
+import { useEffect, useState } from "react";
 
 interface Props {
   game: Game;
@@ -18,40 +20,50 @@ interface Props {
 const GameCard = ({
   game: { name, background_image, rating_top, metacritic },
 }: Props) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    if (!background_image) return;
+
+    setIsLoaded(true);
+  }, [background_image]);
+
   return (
     <Card maxW={300} borderRadius={10} variant="filled" overflow="hidden">
-      <Image
-        objectFit="cover"
-        src={background_image}
-        alt="Green double couch with wooden legs"
-      />
-      <CardBody>
-        <VStack align="left">
-          <HStack justify="space-between">
-            {/* TO-DO: make a separate component */}
-            <HStack>
-              <Icon></Icon>
-              <Icon></Icon>
-              <Icon></Icon>
-              <Icon></Icon>
-              <Icon></Icon>
+      <Skeleton isLoaded={isLoaded}>
+        <Image
+          objectFit="cover"
+          src={background_image}
+          alt="Green double couch with wooden legs"
+        />
+        <CardBody>
+          <VStack align="left">
+            <HStack justify="space-between">
+              {/* TO-DO: make a separate component */}
+              <HStack>
+                <Icon></Icon>
+                <Icon></Icon>
+                <Icon></Icon>
+                <Icon></Icon>
+                <Icon></Icon>
+              </HStack>
+              {/* TO-DO: make a separate component + set color depending on the critic */}
+              <Badge
+                colorScheme="green"
+                fontWeight="bold"
+                fontSize="md"
+                px={2}
+                borderRadius="md"
+              >
+                {metacritic}
+              </Badge>
             </HStack>
-            {/* TO-DO: make a separate component + set color depending on the critic */}
-            <Badge
-              colorScheme="green"
-              fontWeight="bold"
-              fontSize="md"
-              px={2}
-              borderRadius="md"
-            >
-              {metacritic}
-            </Badge>
-          </HStack>
-          <Heading>{name}</Heading>
-          {/* TO-DO: make a separate component + set icon depending on the rating */}
-          <Icon />
-        </VStack>
-      </CardBody>
+            <Heading>{name}</Heading>
+            {/* TO-DO: make a separate component + set icon depending on the rating */}
+            <Icon />
+          </VStack>
+        </CardBody>
+      </Skeleton>
     </Card>
   );
 };
