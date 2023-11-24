9f0337e3b28b3c32215585373829faabf7bf8eb4
Ada <ada@Adas-MacBook-Pro.local>
2023-11-24 20:36:41 +0200

    Added icon to header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,14 @@
-import { HStack, Icon } from "@chakra-ui/react";
+import { HStack, Image, Box } from "@chakra-ui/react";
 
 import Search from "./Search";
 import ModeSwitch from "./ModeSwitch";
 
+import Logo from "../../assets/logo.webp";
+
 const Header = () => {
   return (
     <HStack justifyContent="space-between" alignItems="center" w="100%" py={5}>
-      <Icon />
+      <Image src={Logo} h="60px" />
       <Search />
       <ModeSwitch />
     </HStack>
