13a9f1faf1af286bfed210cd75e4f997261e858f
Ada <ada@Adas-MacBook-Pro.local>
2023-11-24 14:13:37 +0200

    Extended GameCard component

--- src/components/GameCard/GameCard.tsx
+++ src/components/GameCard/GameCard.tsx
@@ -1,14 +1,38 @@
 import { Game } from "../../types/games";
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Image,
+  Text,
+  Stack,
+  Heading,
+  Icon,
+} from "@chakra-ui/react";
+import {} from "@chakra-ui/icons";
 
 interface Props {
   game: Game;
 }
 
-const GameCard = ({ game }: Props) => {
+const GameCard = ({
+  game: { name, background_image, rating_top, metacritic },
+}: Props) => {
   return (
-    <div>
-      <p>{game.name}</p>
-    </div>
+    <Card maxW="sm">
+      <CardBody>
+        <Image
+          src={background_image}
+          alt="Green double couch with wooden legs"
+          borderRadius="lg"
+        />
+        <Stack mt="6" spacing="3">
+          <Text>{metacritic}</Text>
+          <Heading>{name}</Heading>
+          <Icon />
+        </Stack>
+      </CardBody>
+    </Card>
   );
 };
 
