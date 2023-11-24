a2b8e22855fa5d93fe9e711522767b3e00b9d63f
Ada <ada@Adas-MacBook-Pro.local>
2023-11-24 19:24:48 +0200

    Added genres service

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,16 @@ import gamesService from "./services/games-service";
 
 import { VStack, HStack } from "@chakra-ui/react";
 import { Game } from "./types/games";
+import { Genre } from "./types/genres";
 
 import Header from "./components/header/Header";
 import Genres from "./components/genres/Genres";
 import GamesContainer from "./components/games/GamesContainer";
+import genresService from "./services/genres-service";
 
 function App() {
   const [games, setGames] = useState<Game[]>([]);
+  const [genres, setGenres] = useState<Genre[]>([]);
 
   useEffect(() => {
     const { request, cancel } = gamesService.getAllGames();
@@ -25,6 +28,22 @@ function App() {
         // Don't show error is user closed the tab or connection was interrupted
         if (err instanceof CanceledError) return;
         console.error("Failed to fetch games: " + err.message);
+      });
+
+    return () => cancel();
+  }, []);
+
+  useEffect(() => {
+    const { request, cancel } = genresService.getAllGenres();
+
+    request
+      .then((res) => {
+        setGenres(res.data.results);
+      })
+      .catch((err) => {
+        // Don't show error is user closed the tab or connection was interrupted
+        if (err instanceof CanceledError) return;
+        console.error("Failed to fetch genres: " + err.message);
       });
 
     return () => cancel();
